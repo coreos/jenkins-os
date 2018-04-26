6b59d408c077ea596439000fe5b6d5a53c48ddaa
Stephen Lowrie <stephen.lowrie@gmail.com>
2018-04-26 09:03:36 -0500

    os/*: drop OCI

--- os/gc.groovy
+++ os/gc.groovy
@@ -22,16 +22,6 @@ properties([
 the GOOGLE_APPLICATION_CREDENTIALS value for authing to gcloud & creating a bucket''',
                     name: 'GS_CREDS',
                     required: true),
-        credentials(credentialType: 'org.jenkinsci.plugins.plaincredentials.impl.FileCredentialsImpl',
-                    defaultValue: '17942f6a-969b-466d-ac46-cd15925c8953',
-                    description: 'Config required by "ore oci gc"',
-                    name: 'OCI_TEST_CONFIG',
-                    required: true),
-        credentials(credentialType: 'org.jenkinsci.plugins.plaincredentials.impl.FileCredentialsImpl',
-                    defaultValue: 'f7121197-c5b2-4e67-8529-ff59224bef91',
-                    description: 'RSA Key in PEM format referenced by config',
-                    name: 'OCI_TEST_KEY',
-                    required: true),
         credentials(credentialType: 'org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl',
                     defaultValue: 'd67b5bde-d138-487a-9da3-0f5f5f157310',
                     description: 'Credentials with permissions required by "ore packet gc"',
@@ -60,8 +50,6 @@ node('amd64') {
                 secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'],
                 file(credentialsId: params.DIGITALOCEAN_CREDS, variable: 'DIGITALOCEAN_CREDS'),
                 file(credentialsId: params.GS_CREDS, variable: 'GOOGLE_APPLICATION_CREDENTIALS'),
-                file(credentialsId: params.OCI_TEST_CONFIG, variable: 'OCI_TEST_CONFIG'),
-                file(credentialsId: params.OCI_TEST_KEY, variable: 'OCI_TEST_KEY'),
                 string(credentialsId: params.PACKET_CREDS, variable: 'PACKET_API_KEY')
             ]) {
                 withEnv([
@@ -69,21 +57,11 @@ node('amd64') {
                 ]) {
                 sh '''#!/bin/bash -ex
 
-# the oci config file refers to the location of the oci_api_key.pem
-# so we have to move it to the correct location rather than passing
-# in the variables
-rm -rf ~/.oci
-mkdir  --mode=0700 ~/.oci
-mv ${OCI_TEST_CONFIG} ~/.oci/config
-mv ${OCI_TEST_KEY} ~/.oci/oci_api_key.pem
-trap 'rm -rf ~/.oci/' EXIT
-
 timeout --signal=SIGQUIT 60m bin/ore aws gc
 timeout --signal=SIGQUIT 60m bin/ore do gc \
     --config-file="${DIGITALOCEAN_CREDS}"
 timeout --signal=SIGQUIT 60m bin/ore gcloud gc \
     --json-key "${GOOGLE_APPLICATION_CREDENTIALS}"
-timeout --signal=SIGQUIT 60m bin/ore oci gc
 timeout --signal=SIGQUIT 60m bin/ore packet gc \
     --api-key="${PACKET_API_KEY}" \
     --project="${PACKET_PROJECT}" \
